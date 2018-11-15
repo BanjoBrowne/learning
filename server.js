746f2e58bcccf1b75b904519934fd0b5fd48571d
unknown <banjobrowne125@gmail.com>
2018-11-15 21:44:54 +1030

    Updated code to be inline with StandardJS

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-console.log("Server is starting")
+/* globals console */
+'use strict'
+
+console.log('Server is starting')
 
 var express = require('express')
 var app = express()
-var server = app.listen(3000, listening);
+var server = app.listen(3000, listening)
 
-function listening(){
-  console.log("Server Started, Ready to go.")
+function listening () {
+  console.log('Server Started, Ready to go.')
 }
 
-app.use(express.static('website'));
-
+app.use(express.static('website'))
 
 // use command "node server.js"
